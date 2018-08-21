1c04d313a67781ce8806b5a896f71756c1ba533a
Avishay Maor <avishay@guesty.com>
2018-08-21 11:48:43 +0300

    Fix webpack eslint

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -174,7 +174,7 @@ module.exports = {
             loader: require.resolve('babel-loader'),
             options: {
               // @remove-on-eject-begin
-              babelrc: true,
+              babelrc: false,
               presets: [require.resolve('babel-preset-react-app')],
               // @remove-on-eject-end
               compact: true,
