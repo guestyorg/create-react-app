1565086c6bc0ec7046081025cebda2aca316752a
Avishay Maor <avishay@guesty.com>
2018-08-21 12:19:51 +0300

    Fix webpack eslint III

--- packages/react-scripts/template/src/App/App.js
+++ packages/react-scripts/template/src/App/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../assets/images/logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Guesty React Component</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Guesty React Component</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
 
 export default App;
