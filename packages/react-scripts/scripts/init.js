52385efc96947f145eed2c93898a9a649f556c9b
Avishay Maor <avishay@guesty.com>
2018-10-02 10:47:36 +0300

    react-dom fixation

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -38,8 +38,8 @@ module.exports = function(
   // Setup the script rules
   appPackage.scripts = {
     start: 'react-scripts start',
-    build: 'babel ./src --out-dir dist --copy-files --ignore __tests__,spec.js,test.js,__snapshots__',
-    // build: 'react-scripts build',
+    // build: 'babel ./src --out-dir dist --copy-files --ignore __tests__,spec.js,test.js,__snapshots__',
+    build: 'react-scripts build',
     test: 'react-scripts test --env=jsdom',
     eject: 'react-scripts eject',
   };
