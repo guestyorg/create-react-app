42f562429bf6713a9a6d33b8f77e78fd1d18e155
Jonathan yEHIE <jonyehie@gmail.com>
2019-06-17 17:48:42 +0300

    Added support for moment locales

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -374,8 +374,11 @@ module.exports = {
     // by default due to how Webpack interprets its code. This is a practical
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-    // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    // Add all locales required here:
+    new webpack.ContextReplacementPlugin(
+        /moment[\/\\]locale$/,
+        /de|fr|hu|ja|en-us|it|es-es|nl|cs|da|el|hr|hu|pl|ru|sk|pt-br|zh-cn/
+    ),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
